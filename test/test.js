dadd59d757ed6ec103cc5158a3e767eacd3e6227
ETossed <61196008+ETossed@users.noreply.github.com>
2022-12-14 18:21:43 -0600

    Delete test directory

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var removeClass = true;
-let button = document.getElementById("name");
-console.log(button.className);
-button.addEventListener('click', function () {
-    button.style.top = "0%";
-    button.style.fontSize = "4em";
-
-})
