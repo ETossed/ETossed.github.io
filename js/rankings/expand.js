7b1a3b9757442030767e4bacf962fcfe164d4830
ETossed <61196008+ETossed@users.noreply.github.com>
2022-12-15 15:56:45 -0600

    Finished proof of concept and first 8 players

--- js/rankings/expand.js
+++ js/rankings/expand.js
@@ -1,15 +1,24 @@
 // Expanding of player boxes
 
-let currentlyActive = "r1";
+let currentlyActive = "r0";
+const uBoxes = document.querySelectorAll(".u-player");
 
 playerBoxes.forEach(element => {
     element.addEventListener('click', function () {
         let rName = 'r' + element.id;
-
-        resetBox(currentlyActive);
-        currentlyActive = rName;
-
-        document.getElementById(rName).innerHTML = insanelyLongArray[element.id];
+        
+        if (currentlyActive != rName) {
+            resetBox(currentlyActive);
+            currentlyActive = rName;
+            document.getElementById(rName).innerHTML = insanelyLongArray[element.id];
+            if (!currentMode) {
+                let resultTables = document.querySelectorAll(".result-table tr");
+                resultTables.forEach(element => {
+                    element.style.backgroundColor = "#F5F5F5";
+                    element.style.outlineWidth = "2px";
+                })
+            }
+        }
     });
 });
 
@@ -18,4 +27,343 @@ function resetBox (id) {
     return;
 }
 
-let insanelyLongArray = ["", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck", "this array is long as fuck"];
+// DO NOT LOOK BELOW HERE IT'S A MESS
+
+// IM SERIOUS WHAT ARE YOU DOING
+
+// THIS IS ONLY HERE BECAUSE I DON'T HAVE A BACKEND LOL 
+
+// GITHUB PAGES MAKES IT SO I DON'T HAVE TO PAY ANYTHING THO SOOOOOOOOOOOOOOOOOOOOO
+
+
+
+
+
+
+
+
+
+// I WARNED YOU
+
+let zain = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitaional <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">4th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Shine 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Pound 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Apex 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Battle of BC 4 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Get On My Level 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>Phantom 2022 <span class="place">2nd</span></td>
+    </tr>
+</table>`
+
+let mango = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitaional <span class="place">7th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">9th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Super Smash Con 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Mainstage 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 14 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Get On My Level 2022 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Shine 2022 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>Phantom 2022 <span class="place">3rd</span></td>
+    </tr>
+</table>`
+
+let iBDW = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">9th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Double Down 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 13 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Mainstage 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 14 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Apex 2022 <span class="place">4th</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>Phantom 2022 <span class="place">1st</span></td>
+    </tr>
+</table>`
+
+let aMSa = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">7th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Apex 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Double Down 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Mainstage 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Pound 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 14 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>The Off-Season <span class="place">1st</span></td>
+    </tr>
+</table>`
+
+let hungrybox = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Get On My Level 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Riptide 2022 <span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Super Smash Con 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Shine 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Apex 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>Wavedash 2022 <span class="place">1st</span></td>
+    </tr>
+</table>`
+
+let jmook = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">7th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Get On My Level 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Shine 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Riptide 2022 <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 13 <span class="place">4th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Battle of BC 4 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>The Off-Season <span class="place">2nd</span></td>
+    </tr>
+</table>`
+
+let leffen = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">3rd</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">17th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">N/A</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Battle of BC 4<span class="place">1st</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Mainstage 2022 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Pound 2022 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 14 <span class="place">7th</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 13 <span class="place">9th</span></td>
+    </tr>
+    <tr>
+        <td>r</td>
+        <td>Dreamhack Rotterdam <span class="place">1st</span></td>
+    </tr>
+</table>`
+
+let plup = `
+<table class="result-table">
+    <tr>
+        <td>s</td>
+        <td>Genesis 8 <span class="place">13th</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>The Big House 10 <span class="place">N/A</span></td>
+    </tr>
+    <tr>
+        <td>s</td>
+        <td>Ludwig Smash Invitational <span class="place">N/A</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Riptide 2022<span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Lost Tech City 2022 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 13 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>m</td>
+        <td>Smash Summit 14 <span class="place">9th</span></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</table>`
+
+let insanelyLongArray = ["", zain, mango, iBDW, aMSa, hungrybox, jmook, leffen, plup, "this array is long as fuck", "this array is long as fuck"];
+
--- js/rankings/modeChange.js
+++ js/rankings/modeChange.js
@@ -1,12 +1,14 @@
 // WORK IN PROGRESS
 
+let currentMode = 1; // Black = 1, White = 0
 const modeSwitch = document.getElementById("modeswitch");
-
-const playerBoxes = document.querySelectorAll(".u-player");
+const playerBoxes = document.querySelectorAll(".player-container");
 const rankingBoxes = document.querySelectorAll(".ranking-box");
+
 
 function modeChange() {
     if (document.body.style.backgroundColor == "rgb(250, 250, 250)") {
+        currentMode = 1;
         document.documentElement.style.setProperty("--mouseover-color", "#FAFAFA");
         document.body.style.backgroundColor = "#424242";
 
@@ -21,8 +23,14 @@ function modeChange() {
             element.style.outlineWidth = "0px";
         });
 
+        let resultTables = document.querySelectorAll(".result-table tr");
+        resultTables.forEach(element => {
+            element.style.backgroundColor = "#323232";
+            element.style.outlineWidth = "0px";
+        })
     }
     else {
+        currentMode = 0;
         document.documentElement.style.setProperty("--mouseover-color", "#424242");
         document.body.style.backgroundColor = "#FAFAFA";
 
@@ -36,6 +44,12 @@ function modeChange() {
             element.style.backgroundColor = "#F5F5F5";
             element.style.outlineWidth = "2px";
         });
+
+        let resultTables = document.querySelectorAll(".result-table tr");
+        resultTables.forEach(element => {
+            element.style.backgroundColor = "#F5F5F5";
+            element.style.outlineWidth = "2px";
+        })
 
     }
 }
