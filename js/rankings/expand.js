894bf8ca69b07069075bc6669d5b08360bf08181
ETossed <61196008+ETossed@users.noreply.github.com>
2022-12-20 15:08:18 -0600

    Added kins0

--- js/rankings/expand.js
+++ js/rankings/expand.js
@@ -699,11 +699,26 @@ let hundredgrand = `
         <td>Minnesota Monthly #5 <span class="place">7th</span></td>
     </tr>
 </table>`
+let kinso = `
+<table class="result-table">
+    <tr>
+        <td>&#9650;</td>
+        <td>MYTH 3 <span class="place">2nd</span></td>
+    </tr>
+    <tr>
+        <td>&#9650;</td>
+        <td>Dreamhack Rotterdam 2022 <span class="place">5th</span></td>
+    </tr>
+    <tr>
+        <td>&#9650;</td>
+        <td>HFLAN Melee Edition 2022 <span class="place">5th</span></td>
+    </tr>
+</table>`
 
 let insanelyLongArray = ["", zain, mango, aMSa, iBDW, hungrybox, jmook, leffen, plup, 
 slug, axe, llod, kodorin, moky, fiction, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 
 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 
 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 
-97, 98, 99, 100, 101, 102, 103, 104, 105, nicki, inngenn, akir, alex, hundredgrand];
+97, 98, 99, 100, 101, 102, 103, 104, 105, nicki, inngenn, akir, alex, hundredgrand, kinso];
 
