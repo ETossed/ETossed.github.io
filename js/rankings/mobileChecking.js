5d244b72cae4f4a7907ab861e3062309e377d2e3
ETossed <61196008+ETossed@users.noreply.github.com>
2022-12-20 01:52:03 -0600

    Added more mobile changes

--- js/rankings/mobileChecking.js
+++ js/rankings/mobileChecking.js
@@ -3,8 +3,11 @@ const gamertags = document.querySelectorAll(".gamertag");
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     playerBoxes.forEach(element => {
         element.style.width = "80%";
-    })
+    });
+    blurbs.forEach(element => {
+        element.style.width = "70%";
+    });
     gamertags.forEach(element => {
         element.style.fontWeight = "500";
-    })
+    });
 }
